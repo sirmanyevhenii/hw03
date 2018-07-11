dc85e18b0470cae159d275780add95bcb00ff941
Yevhenii Sirman <sirmanj92@gmail.com>
2018-07-11 12:59:22 +0000

    fix css and js

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,21 @@
 console.log('Hello, I wish you a good day');
 
+
+// burger menu
 $('.toggle-btn').click(function() {
 	$('.top-menu').toggleClass('top-menu--active');
-	$('.toggle-btn__top').toggleClass('toggle-btn__top--active');
-	$('.toggle-btn__middle').toggleClass('toggle-btn__middle--active');
-	$('.toggle-btn__bottom').toggleClass('toggle-btn__bottom--active');
+	$('.toggle-btn').toggleClass('active');
 });
 
 $(document).click(function(event) {
 	if ($(event.target).closest('.toggle-btn').length ) return;
-	$('.toggle-btn__top').removeClass('toggle-btn__top--active');
-	$('.toggle-btn__middle').removeClass('toggle-btn__middle--active');
-	$('.toggle-btn__bottom').removeClass('toggle-btn__bottom--active');
+	$('.toggle-btn').removeClass('active');
 	$('.top-menu').removeClass('top-menu--active');
 	event.stopPropagation();
 });
 
 
-
+// scroll
 $(function() {
     $('.smooth').on('click', function(event) {
         var target = $(this.getAttribute('href'));
